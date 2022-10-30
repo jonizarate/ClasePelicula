86883805fd26c92eba23c6b3acf16116037da112
jonizarate <jonizarate00@gmail.com>
2022-10-30 09:15:32 -0300

    Agregar archivos de proyecto.

---
+++ ClassPelicula.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePelicula
+{
+    internal class ClassPelicula
+    {
+        public string Nombre { get; set; }
+        public string Director { get; set; }
+        public int AnioDeLiberacion { get; set; }
+        public string Genero { get; set; }
+        public int Presupuesto { get; set; }
+
+        public int Recaudacion { get; set; }
+        public short DuracionEnMinutos { get; set; }
+        
+        
+        public ClassPelicula()
+        {
+            Nombre = "";
+            Director = "";
+            AnioDeLiberacion = 0;
+            Genero = "";
+            Presupuesto = 0;
+            Recaudacion = 0;
+            DuracionEnMinutos = 0;
+        }
+        public ClassPelicula(string nombre, string director, int anioDeLiberacion, string genero, int presupuesto, int recaudacion, short duracionEnMinutos)
+        {
+            Nombre = nombre;
+            Director = director;
+            AnioDeLiberacion = anioDeLiberacion;
+            Genero = genero;
+            Presupuesto = presupuesto;
+            Recaudacion = recaudacion;
+            DuracionEnMinutos = duracionEnMinutos;
+        }
+
+        public bool EsAnioBisiesto()
+        {
+            if (AnioDeLiberacion % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool EsDeTerror()
+        {
+            return Genero.ToLower().Contains("terror");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using ClasePelicula;
+
+ClassPelicula miPelicula =new ClassPelicula();
+
+Console.WriteLine("Ïngresa el nombre de la pelicula: ");
+miPelicula.Nombre = Console.ReadLine();
+Console.WriteLine("Ïngresa el nombre del director: ");
+miPelicula.Director = Console.ReadLine();
+Console.WriteLine("Ïngresa el año de liberacion: ");
+miPelicula.AnioDeLiberacion = int.Parse(Console.ReadLine());
+Console.WriteLine("Ïngresa el Genero: ");
+miPelicula.Genero = Console.ReadLine();
+Console.WriteLine("Ïngresa el presupuesto de la pelicula: ");
+miPelicula.Presupuesto =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ïngresa la recaudacion total: ");
+miPelicula.Recaudacion =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ingresa la Duracion total en minutos de la pelicula: ");
+miPelicula.DuracionEnMinutos = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("El nombre de tu pelicula es: " + miPelicula.Nombre + " El nombre del director: "+miPelicula.Director+" El año de liberacion" +
+    " de la pelicula fue: " + miPelicula.AnioDeLiberacion + " Genero: " +miPelicula.Genero +" Presupuesto en dolares: "+miPelicula.Presupuesto+" Recaudacion en dolares: " +
+    miPelicula.Recaudacion+" Duracion en minutos: " + miPelicula.Recaudacion);
+
+
+if (miPelicula.EsDeTerror())
+{
+    Console.WriteLine("Efectivamente es de terror");
+}
+else
+{
+    Console.WriteLine("Realmente no es de terror");
+}
